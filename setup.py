681096baeee2edb8cfc8656503366ddb91c13c88
bi-jvo <jvo@bi-web.de>
2022-11-30 11:17:09 +0100

    v.1.0.1 - Enable weekly dependency checks

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="fuel_http_helper",
-    version="1.0.0",
+    version="1.0.1",
     author="bi-jvo",
     author_email="jvo@bi-web.de",
     description="Automatically uploads files to a Trilith FuelHttpServer",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["requests"],
+    install_requires=required,
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3",
